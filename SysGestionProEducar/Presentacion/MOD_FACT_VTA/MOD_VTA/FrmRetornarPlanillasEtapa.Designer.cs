d57dc8c60eb3ed8cbc6629b4163a2263d1910c61
Odair-ODR <odair.1697@gmail.com>
2023-09-17 21:37:31 -0500

    retorno de planillas a cualquier etapa, sin restricciones

---
+++ SysGestionProEducar/Presentacion/MOD_FACT_VTA/MOD_VTA/FrmRetornarPlanillasEtapa.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Presentacion.MOD_FACT_VTA.MOD_VTA
+{
+    partial class FrmRetornarPlanillasEtapa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboEtapa = new System.Windows.Forms.ComboBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Etapa";
+            // 
+            // cboEtapa
+            // 
+            this.cboEtapa.FormattingEnabled = true;
+            this.cboEtapa.Location = new System.Drawing.Point(91, 62);
+            this.cboEtapa.Name = "cboEtapa";
+            this.cboEtapa.Size = new System.Drawing.Size(239, 21);
+            this.cboEtapa.TabIndex = 1;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(142, 122);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(115, 35);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // FrmRetornarPlanillasEtapa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 184);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.cboEtapa);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmRetornarPlanillasEtapa";
+            this.Load += new System.EventHandler(this.FrmRetornarPlanillasEtapa_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboEtapa;
+        private System.Windows.Forms.Button btnAceptar;
+    }
+}
---
+++ SysGestionProEducar/Presentacion/MOD_FACT_VTA/MOD_VTA/FrmRetornarPlanillasEtapa.cs
@@ -0,0 +1,64 @@
+﻿using BLL;
+using System;
+using System.Data;
+using System.Windows.Forms;
+using Entidades;
+using Presentacion.HELPERS;
+
+namespace Presentacion.MOD_FACT_VTA.MOD_VTA
+{
+    public partial class FrmRetornarPlanillasEtapa : Form
+    {
+        private readonly int idSeguimiento, idEstado;
+        public FrmRetornarPlanillasEtapa(int idSeguimiento, int idEstado)
+        {
+            InitializeComponent();
+            this.idEstado = idEstado;
+            this.idSeguimiento = idSeguimiento;
+        }
+
+        private static readonly SeguimientoPlanillasBLL BLSeguimiento = new SeguimientoPlanillasBLL();
+
+        private void FrmRetornarPlanillasEtapa_Load(object sender, EventArgs e)
+        {
+            StartControls();
+            CargarEtapas();
+        }
+
+        private void StartControls()
+        {
+            cboEtapa.DropDownStyle = ComboBoxStyle.DropDownList; 
+        }
+
+        private void CargarEtapas()
+        {
+            DataTable dt = BLSeguimiento.ListarEtapasPlanillaParaRetroceso(this.idEstado);
+            cboEtapa.DataSource = dt;
+            cboEtapa.ValueMember = "ID_ESTADO";
+            cboEtapa.DisplayMember = "DESC_CORTA";
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            FrmConfirmar frmConfirmar = new FrmConfirmar(TIPO_CONFIRMAR.RETORNAR_ETAPA_PROCESO)
+            {
+                StartPosition = FormStartPosition.CenterParent
+            };
+            frmConfirmar.EventClick += FrmConfirmar_EventClick;
+            _ = frmConfirmar.ShowDialog(this);
+        }
+
+        private void FrmConfirmar_EventClick(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("¿Esta seguro de que desea regresar esta planilla a la etapa anterior?", "MESSAGE", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dr == DialogResult.Yes)
+            {
+                int idEstadoAnterior = cboEtapa.SelectedValue.ToInt32();
+                _ = BLSeguimiento.RegresarEstadoAnterior(idSeguimiento, idEstado, idEstadoAnterior)
+                    ? MessageBox.Show("El cambio se a efectuado correctamente", "MESSAGE", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    : MessageBox.Show("Ocurrió un error inesperado", "MESSAGE", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+    }
+}
